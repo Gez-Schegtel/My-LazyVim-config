ed05ec64bb1003a12ef40cee484742057611b67e
Juani <gez.schegtel_ji@proton.me>
2025-05-08 22:15:58 -0300

    Hay un bug con mason en esta actualización de LazyVim. Añadí un arreglo temporal con el "plugin" "mason-temp-workaround.lua"

---
+++ lua/plugins/mason-temp-workaround.lua
@@ -0,0 +1,5 @@
+-- This is a Bug between LazyVim and Mason. It should be fixed by the developers.
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
