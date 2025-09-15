bfca4e3599f48985489846d79274296ae9c11bbd
Juani <gez.schegtel_ji@proton.me>
2025-09-15 03:12:33 -0300

    Nuevos plugins añadidos.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.diagnostic.enable(false) -- Desactivo los diagnósticos al inicio
+vim.opt.cursorline = false -- Desactivé el highlight de la línea donde está el cursor.
+vim.opt.spelllang = { "en", "es" } -- Con esto podemos agregar al corrector ortográfico los idiomas inglés y español al mismo tiempo.
